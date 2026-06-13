e9b5d69667a7e41e7f18cf5671348b137fa1a97b
varadkharat72 <kharatvarad12@gmail.com>
2026-06-13 21:05:30 +0530

    first commit

---
+++ server/controllers/comment.js
@@ -0,0 +1,54 @@
+import comment from "../models/comment.js";
+import mongoose from "mongoose";
+
+export const postcomment = async (req, res) => {
+  const commentdata = req.body;
+  const postcomment = new comment(commentdata);
+  try {
+    await postcomment.save();
+    return res.status(200).json({ comment: true });
+  } catch (error) {
+    console.error(" error:", error);
+    return res.status(500).json({ message: "Something went wrong" });
+  }
+};
+export const getallcomment = async (req, res) => {
+  const { videoid } = req.params;
+  try {
+    const commentvideo = await comment.find({ videoid: videoid });
+    return res.status(200).json(commentvideo);
+  } catch (error) {
+    console.error(" error:", error);
+    return res.status(500).json({ message: "Something went wrong" });
+  }
+};
+export const deletecomment = async (req, res) => {
+  const { id: _id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(_id)) {
+    return res.status(404).send("comment unavailable");
+  }
+  try {
+    await comment.findByIdAndDelete(_id);
+    return res.status(200).json({ comment: true });
+  } catch (error) {
+    console.error(" error:", error);
+    return res.status(500).json({ message: "Something went wrong" });
+  }
+};
+
+export const editcomment = async (req, res) => {
+  const { id: _id } = req.params;
+  const { commentbody } = req.body;
+  if (!mongoose.Types.ObjectId.isValid(_id)) {
+    return res.status(404).send("comment unavailable");
+  }
+  try {
+    const updatecomment = await comment.findByIdAndUpdate(_id, {
+      $set: { commentbody: commentbody },
+    });
+    res.status(200).json(updatecomment);
+  } catch (error) {
+    console.error(" error:", error);
+    return res.status(500).json({ message: "Something went wrong" });
+  }
+};
