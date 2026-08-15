0aa24c162fbfe990fc6f7ee1120f57bf309e9866
varadkharat72 <kharatvarad12@gmail.com>
2026-08-15 11:28:01 +0530

    Fixed Premium Section

--- server/controllers/premium.js
+++ server/controllers/premium.js
@@ -110,6 +110,7 @@ export const verifyPayment = async (req, res) => {
     return res.status(200).json({
       success: true,
       message: "Premium Activated",
+      plan: plan,
       user,
     });
   } catch (error) {
